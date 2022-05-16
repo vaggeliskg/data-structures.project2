aa3d385b2685e54ed0afc3499c997cc1e11aa771
vaggeliskg <sdi2100045@di.uoa.gr>
2022-05-16 14:51:27 +0300

    project 2

--- programs/queue_benchmark/queue_benchmark.c
+++ programs/queue_benchmark/queue_benchmark.c
@@ -21,22 +21,60 @@ int main(int argc, char **argv) {
     int remove = 0;
     int sum_real_time = 0;
     int sum_amortized_time = 0;
-
-    for(int i = 0 ; i < 9000 ; i++) {
-        if(insert1 == 1) {
-            Pointer value = create_int(i);
-            queue_insert_back(queue,value);
-            sum_amortized_time += queue_steps(queue);
-            insert1 = 0;
-            insert2 = 1;
-            if(strcmp(argv[1], "amortized") == 0) {
+    if(strcmp(argv[1], "amortized") == 0) {
+        for(int i = 0 ; i < 9000 ; i++) {
+            if(insert1 == 1) {
+                Pointer value = create_int(i);
+                queue_insert_back(queue,value);
+                sum_amortized_time += queue_steps(queue);
+                insert1 = 0;
+                insert2 = 1;
+                int amortized_steps = sum_amortized_time / (i+1);
+                    fprintf(filepntr, "%d,%d\n", i+1, amortized_steps);
+            }
+            continue;
+            if(insert2 == 1) {
+                Pointer value = create_int(i);
+                queue_insert_back(queue,value);
+                sum_amortized_time +=queue_steps(queue);
+                insert1 = 1;
+                insert2 = 0;
+                remove = 1;
                 int amortized_steps = sum_amortized_time / (i+1);
                 fprintf(filepntr, "%d,%d\n", i+1, amortized_steps);
             }
-            continue;
+            if(remove == 1) {
+                queue_remove_front(queue);
+                sum_amortized_time += queue_steps(queue);
+                remove = 0;
+                int amortized_steps = sum_amortized_time / (i+1);
+                 fprintf(filepntr, "%d,%d\n", i+1, amortized_steps);
+            }
         }
-
-
     }
-
+    else if(strcmp(argv[1], "real") == 0) {
+        for(int i = 0 ; i < 9000 ; i++) {
+            if(insert1 == 1) {
+                Pointer value = create_int(i);
+                queue_insert_back(queue,value);
+                insert1 = 0;
+                insert2 = 1;
+            }
+            continue;
+            if(insert2 == 1) {
+                Pointer value = create_int(i);
+                queue_insert_back(queue,value);
+                insert1 = 1;
+                insert2 = 0;
+                remove = 1;
+            }
+            if(remove == 1) {
+                queue_remove_front(queue);
+                sum_real_time += queue_steps(queue);
+                remove = 0;
+                int real_steps = sum_real_time;
+                 fprintf(filepntr, "%d,%d\n", i+1, real_steps);
+            }
+        } 
+    }
 }
